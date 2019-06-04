df5dfe342a9e474a65702c5aa57c8c0cc510b1e0
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-04 21:26:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,18 +2,17 @@
 
 def countdown(int)
   while int > 0
-    puts "#{n} SECOND(S)!"
+    puts "#{int} SECOND(S)!"
     int -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(int)
-  n = int
-  while n > 0
-    puts "#{n} SECOND(S)!"
+  while int > 0
+    puts "#{int} SECOND(S)!"
     sleep (1)
-    n -= 1
+    int -= 1
   end
   "HAPPY NEW YEAR!"
 end
